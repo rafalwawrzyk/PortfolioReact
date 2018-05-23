0a54ff0577060d117bb0168897de73268f48af96
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2018-05-23 21:53:38 +0200

    add fonts, title,subtitle

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,23 +1,26 @@
-import React,{Component} from 'react';
-import background from '../../assets/images/photo3.jpeg'
-import styles from './Main.css'
+import React, { Component } from 'react';
+import background from '../../assets/images/photo3.jpeg';
+import styles from './Main.css';
 
+/* eslint-disable*/
 
-
-
-/* eslint-disable*/ 
-
-
-const Main =  () => {
-  return (
-    <div className={styles.fullpageBg}>
-      
-    </div>
-  )
-}
-
-
-
-
+const Main = () => {
+	return (
+		<div className={styles.fullpageBg}>
+			<div className={styles.container}>
+				<h1 className={styles.title}>
+					<span className={styles.colorMain}>front</span> <br />
+					<span className={styles.colorMain}> end</span>
+					<span> dev.</span>
+					<br />
+				</h1>
+                <p className={styles.subtext}>passion through the code</p>
+                <div className={styles.btnCont}>
+                    <button>Check</button>
+                </div>
+			</div>
+		</div>
+	);
+};
 
 export default Main;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import styles from './NavStyle.css';
+import logo from '../../assets/images/logo.jpg'
 
 class Nav extends Component {
 	render() {
 		return (
 			<div className={styles.fixedMenu}>
 				<div className={styles.container}>
-        <div>
-          LOGO
-        </div>
 					<div>
+            <img src={logo} alt="" className={styles.imageLogo}/>
+          </div>
+					<div className={styles.burgerCont}>
 						<div className={styles.burgerRound}>
 							{/* <ul className={styles.navReset}>
           <li>About</li>
