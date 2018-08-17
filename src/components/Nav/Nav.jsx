0e0249fe5686b5c6cdd5df71886c5fdf0d93efae
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2018-08-17 15:12:41 +0200

    add delays and add new route home

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './NavStyle.css';
-import logo from '../../assets/images/logo.jpg';
-import {TimelineLite} from 'gsap';
-import {NavLink} from 'react-router-dom'
-import {animation} from '../../animations/animation'
+import logo from '../../assets/images/logo3.jpg';
+import { TimelineLite } from 'gsap';
+import { NavLink } from 'react-router-dom';
+import { animation } from '../../animations/animation';
 import classNames from 'classnames';
 
 class Nav extends Component {
@@ -12,53 +12,58 @@ class Nav extends Component {
 		this.onEnter = this.onEnter.bind(this);
 		this.onLeave = this.onLeave.bind(this);
 		this.state = {
-			tl:new TimelineLite({paused:true})
-		}
+			tl: new TimelineLite({ paused: true })
+		};
 	}
 
-
-
-
 	onEnter() {
-		animation.show({
-			tl:this.state.tl,
-			elem:this.refs.burgerLines,
-			elem2:this.refs.round,
-			elem3:this.refs.list})
+		animation
+			.show({
+				tl: this.state.tl,
+				elem: this.refs.burgerLines,
+				elem2: this.refs.round,
+				elem3: this.refs.list
+			})
 			.play();
 	}
 
-	onLeave(){
-		animation.show({
-			tl:this.state.tl,
-			elem:this.refs.burgerLines,
-			elem2:this.refs.round,
-			elem3:this.refs.list})
+	onLeave() {
+		animation
+			.show({
+				tl: this.state.tl,
+				elem: this.refs.burgerLines,
+				elem2: this.refs.round,
+				elem3: this.refs.list
+			})
 			.reverse();
 	}
 
 	render() {
 		return (
-			<div className='fixedMenu'>
-					<div>
-						<NavLink to='/'>
-						<img src={logo} alt="" className='imageLogo' ref="logo" />
-						</NavLink>
-					</div>
-					<div className='burgerCont' onMouseEnter={this.onEnter} onMouseLeave={this.onLeave}>
-						<div className='burgerRound' ref='round'>
-							<ul className='navReset' ref='list'>
-								<NavLink to="/about" activeClassName='isActive' className='decorLink'><li>About</li></NavLink>
-								<NavLink to='/projects' activeClassName='isActive' className='decorLink'><li>Projects</li></NavLink>
-								<NavLink to='/contact'activeClassName='isActive'  className='decorLink'><li>Contact</li></NavLink>
-							</ul>
-							<div className='burgerLineCont' ref='burgerLines'>
-								<div className='burgerLine' />
-								<div className={classNames('burgerLine', 'spaced')} />
-								<div className='burgerLine' />
-							</div>
+			<div className="fixedMenu">
+				<div className="burgerCont" onMouseEnter={this.onEnter} onMouseLeave={this.onLeave}>
+					<div className="burgerRound" ref="round">
+						<ul className="navReset" ref="list">
+							<NavLink to="/" activeClassName="isActive" className="decorLink">
+								<li>Home</li>
+							</NavLink>
+							<NavLink to="/about" activeClassName="isActive" className="decorLink">
+								<li>About</li>
+							</NavLink>
+							<NavLink to="/projects" activeClassName="isActive" className="decorLink">
+								<li>Projects</li>
+							</NavLink>
+							<NavLink to="/contact" activeClassName="isActive" className="decorLink">
+								<li>Contact</li>
+							</NavLink>
+						</ul>
+						<div className="burgerLineCont" ref="burgerLines">
+							<div className="burgerLine" />
+							<div className={classNames('burgerLine', 'spaced')} />
+							<div className="burgerLine" />
 						</div>
 					</div>
+				</div>
 			</div>
 		);
 	}
